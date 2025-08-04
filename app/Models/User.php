559f3fa300966642e6a286da95d4e27237b72b1c
doanduyvan <duyvanlee2001@gmail.com>
2025-08-04 23:16:10 +0700

    up

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,29 +28,10 @@ class User extends Authenticatable
 
    protected $hidden = [
         'password',
-        'remember_token',
     ];
 
         protected $casts = [
         'is_active' => 'boolean',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
